5e2a622154cd4e88eaeed3763406efccbc3d6fff
Stephen Gutekanst <stephen@sourcegraph.com>
2022-04-25 15:09:07 -0700

    frontend: homepage now lists projects
    Signed-off-by: Stephen Gutekanst <stephen@sourcegraph.com>

--- frontend/src/Pages/Home_.elm
+++ frontend/src/Pages/Home_.elm
@@ -1,11 +1,14 @@
 module Pages.Home_ exposing (Model, Msg, page)
 
+import Element as E
 import Gen.Params.Home_ exposing (Params)
+import Http
+import Json.Decode as D
 import Page
 import Request
 import Shared
 import View exposing (View)
-import Html
+
 
 page : Shared.Model -> Request.With Params -> Page.With Model Msg
 page shared req =
@@ -22,12 +25,12 @@ page shared req =
 
 
 type alias Model =
-    {}
+    { list : Maybe (Result Http.Error (List String)) }
 
 
 init : ( Model, Cmd Msg )
 init =
-    ( {}, Cmd.none )
+    ( { list = Nothing }, fetchList )
 
 
 
@@ -35,14 +38,22 @@ init =
 
 
 type Msg
-    = ReplaceMe
+    = GotList (Result Http.Error (List String))
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        ReplaceMe ->
-            ( model, Cmd.none )
+        GotList list ->
+            ( { model | list = Just list }, Cmd.none )
+
+
+fetchList : Cmd Msg
+fetchList =
+    Http.get
+        { url = "/api/list"
+        , expect = Http.expectJson GotList (D.list D.string)
+        }
 
 
 
@@ -61,5 +72,19 @@ subscriptions model =
 view : Model -> View Msg
 view model =
     { title = "doctree"
-    , body = [ Html.text "Hello, doctree!" ]
+    , body =
+        [ E.layout []
+            (case model.list of
+                Just response ->
+                    case response of
+                        Ok list ->
+                            E.row [] (List.map (\projectName -> E.link [] { url = projectName, label = E.text projectName }) list)
+
+                        Err err ->
+                            E.text (Debug.toString err)
+
+                Nothing ->
+                    E.text "loading.."
+            )
+        ]
     }

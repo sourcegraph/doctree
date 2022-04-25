251d9a2ef7bc9658ba28486f0b97b7fd28ed0543
Stephen Gutekanst <stephen@sourcegraph.com>
2022-04-25 14:13:06 -0700

    frontend: change to element page
    Signed-off-by: Stephen Gutekanst <stephen@sourcegraph.com>

--- frontend/src/Pages/Home_.elm
+++ frontend/src/Pages/Home_.elm
@@ -1,11 +1,65 @@
-module Pages.Home_ exposing (view)
+module Pages.Home_ exposing (Model, Msg, page)
 
-import Html
+import Gen.Params.Home_ exposing (Params)
+import Page
+import Request
+import Shared
 import View exposing (View)
+import Html
+
+page : Shared.Model -> Request.With Params -> Page.With Model Msg
+page shared req =
+    Page.element
+        { init = init
+        , update = update
+        , view = view
+        , subscriptions = subscriptions
+        }
 
 
-view : View msg
-view =
+
+-- INIT
+
+
+type alias Model =
+    {}
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( {}, Cmd.none )
+
+
+
+-- UPDATE
+
+
+type Msg
+    = ReplaceMe
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        ReplaceMe ->
+            ( model, Cmd.none )
+
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
+
+
+-- VIEW
+
+
+view : Model -> View Msg
+view model =
     { title = "doctree"
     , body = [ Html.text "Hello, doctree!" ]
     }

b016075cb8721ce1f91fb680dbf2d656660e8f15
Stephen Gutekanst <stephen@sourcegraph.com>
2022-04-25 15:49:05 -0700

    frontend: $ elm-spa add /not-found element
    Signed-off-by: Stephen Gutekanst <stephen@sourcegraph.com>

--- frontend/src/Pages/NotFound.elm
+++ frontend/src/Pages/NotFound.elm
@@ -1,12 +1,63 @@
-module Pages.NotFound exposing (view)
+module Pages.NotFound exposing (Model, Msg, page)
 
+import Gen.Params.NotFound exposing (Params)
+import Page
+import Request
+import Shared
 import View exposing (View)
 
 
+page : Shared.Model -> Request.With Params -> Page.With Model Msg
+page shared req =
+    Page.element
+        { init = init
+        , update = update
+        , view = view
+        , subscriptions = subscriptions
+        }
 
--- TODO: nicer NotFound page
 
 
-view : View msg
-view =
-    View.placeholder "Page not found."
+-- INIT
+
+
+type alias Model =
+    {}
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( {}, Cmd.none )
+
+
+
+-- UPDATE
+
+
+type Msg
+    = ReplaceMe
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        ReplaceMe ->
+            ( model, Cmd.none )
+
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
+
+
+-- VIEW
+
+
+view : Model -> View Msg
+view model =
+    View.placeholder "NotFound"

be0be722ca02989fb036641bd4879c799690d75d
Stephen Gutekanst <stephen@sourcegraph.com>
2022-04-25 21:50:15 -0700

    frontend: basic project page
    Signed-off-by: Stephen Gutekanst <stephen@sourcegraph.com>

--- frontend/src/ProjectPage.elm
+++ frontend/src/ProjectPage.elm
@@ -1,21 +1,59 @@
 module ProjectPage exposing (Model, Msg, init, page, subscriptions, update, view)
 
+import Dict
+import Element as E
 import Gen.Params.NotFound exposing (Params)
+import Html exposing (Html)
+import Http
 import Page
-import Request exposing (Request)
+import Request
+import Schema
 import Shared
-import Url
+import Url.Builder
 import View exposing (View)
 
 
 page : Shared.Model -> Request.With Params -> Page.With Model Msg
 page shared req =
+    let
+        rawProjectURI =
+            String.dropLeft (String.length "/") req.url.path
+
+        projectURI =
+            parseProjectURI rawProjectURI
+    in
     Page.element
-        { init = init
+        { init = init projectURI
         , update = update
-        , view = view req
+        , view = view projectURI
         , subscriptions = subscriptions
         }
+
+
+type ProjectURI
+    = Name String
+    | NameLanguage String String
+    | NameLanguagePage String String String
+    | NameLanguagePageSection String String String String
+
+
+parseProjectURI : String -> Maybe ProjectURI
+parseProjectURI uri =
+    case String.split "/-/" uri of
+        name :: language :: docPage :: section :: _ ->
+            Just (NameLanguagePageSection name language docPage section)
+
+        name :: language :: docPage :: _ ->
+            Just (NameLanguagePage name language docPage)
+
+        name :: language :: _ ->
+            Just (NameLanguage name language)
+
+        name :: _ ->
+            Just (Name name)
+
+        _ ->
+            Nothing
 
 
 
@@ -23,12 +61,42 @@ page shared req =
 
 
 type alias Model =
-    {}
+    { projectIndexes : Maybe (Result Http.Error Schema.ProjectIndexes) }
 
 
-init : ( Model, Cmd Msg )
-init =
-    ( {}, Cmd.none )
+init : Maybe ProjectURI -> ( Model, Cmd Msg )
+init projectURI =
+    ( { projectIndexes = Nothing }
+    , case projectURI of
+        Just uri ->
+            fetchProject uri
+
+        Nothing ->
+            Cmd.none
+    )
+
+
+fetchProject : ProjectURI -> Cmd Msg
+fetchProject projectURI =
+    let
+        projectName =
+            case projectURI of
+                Name v ->
+                    v
+
+                NameLanguage v _ ->
+                    v
+
+                NameLanguagePage v _ _ ->
+                    v
+
+                NameLanguagePageSection v _ _ _ ->
+                    v
+    in
+    Http.get
+        { url = Url.Builder.absolute [ "api", "get" ] [ Url.Builder.string "name" projectName ]
+        , expect = Http.expectJson GotProject Schema.projectIndexesDecoder
+        }
 
 
 
@@ -36,14 +104,14 @@ init =
 
 
 type Msg
-    = ReplaceMe
+    = GotProject (Result Http.Error Schema.ProjectIndexes)
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        ReplaceMe ->
-            ( model, Cmd.none )
+        GotProject projectIndexes ->
+            ( { model | projectIndexes = Just projectIndexes }, Cmd.none )
 
 
 
@@ -59,6 +127,63 @@ subscriptions model =
 -- VIEW
 
 
-view : Request -> Model -> View Msg
-view req model =
-    View.placeholder (String.dropLeft (String.length "/") req.url.path)
+view : Maybe ProjectURI -> Model -> View Msg
+view projectURI model =
+    { title = "TODO"
+    , body =
+        [ case model.projectIndexes of
+            Just response ->
+                case response of
+                    Ok projectIndexes ->
+                        case projectURI of
+                            Just uri ->
+                                case uri of
+                                    NameLanguagePageSection name language docPage section ->
+                                        -- viewNameLanguagePageSection model name language docPage section
+                                        viewName model projectIndexes name
+
+                                    NameLanguagePage name language docPage ->
+                                        -- viewNameLanguagePage model name language docPage
+                                        viewName model projectIndexes name
+
+                                    NameLanguage name language ->
+                                        viewNameLanguage model projectIndexes name language
+
+                                    Name name ->
+                                        viewName model projectIndexes name
+
+                            Nothing ->
+                                E.layout [] (E.text "loading..")
+
+                    Err err ->
+                        E.layout [] (E.text (Debug.toString err))
+
+            Nothing ->
+                E.layout [] (E.text "loading..")
+        ]
+    }
+
+
+viewName : Model -> Schema.ProjectIndexes -> String -> Html Msg
+viewName model projectIndexes projectName =
+    E.layout []
+        (E.row []
+            (List.map (\language -> E.link [] { url = String.concat [ Url.Builder.absolute [ projectName, "-", language ] [] ], label = E.text language }) (Dict.keys projectIndexes))
+        )
+
+
+viewNameLanguage : Model -> Schema.ProjectIndexes -> String -> String -> Html Msg
+viewNameLanguage model projectIndexes projectName language =
+    let
+        indexLookup =
+            Dict.get language projectIndexes
+    in
+    case indexLookup of
+        Just index ->
+            E.layout []
+                (E.row []
+                    [ E.text index.schemaVersion ]
+                )
+
+        Nothing ->
+            E.layout [] (E.text "language not found")

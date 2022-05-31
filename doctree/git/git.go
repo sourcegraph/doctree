523c7b8c901f5e46de7a7eeba0722cd0b7bcc2f0
Stephen Gutekanst <stephen@sourcegraph.com>
2022-05-30 17:10:56 -0700

    doctree/git: move Git invocation helpers to library
    Signed-off-by: Stephen Gutekanst <stephen@sourcegraph.com>

---
+++ doctree/git/git.go
@@ -0,0 +1,46 @@
+package git
+
+import (
+	"fmt"
+	"net/url"
+	"os/exec"
+	"path/filepath"
+	"strings"
+
+	"github.com/pkg/errors"
+)
+
+func URIForFile(dir string) (string, error) {
+	cmd := exec.Command("git", "config", "--get", "remote.origin.url")
+	absDir, err := filepath.Abs(dir)
+	if err != nil {
+		return "", errors.Wrapf(err, "failed to get absolute path for %s", dir)
+	}
+	cmd.Dir = absDir
+	out, err := cmd.Output()
+	if err != nil {
+		return "", errors.Wrapf(err, "git config --get remote.origin.url (pwd=%s)", cmd.Dir)
+	}
+	gitURL, err := normalizeGitURL(strings.TrimSpace(string(out)))
+	if err != nil {
+		return "", errors.Wrap(err, "normalizeGitURL")
+	}
+	return gitURL, nil
+}
+
+func normalizeGitURL(gitURL string) (string, error) {
+	s := gitURL
+	if strings.HasPrefix(gitURL, "git@") {
+		s = strings.Replace(s, ":", "/", 1)
+		s = strings.Replace(s, "git@", "git://", 1) // dummy scheme
+	}
+
+	u, err := url.Parse(s)
+	if err != nil {
+		return "", err
+	}
+	p := u.Path
+	p = strings.TrimSuffix(p, ".git")
+	p = strings.TrimPrefix(p, "/")
+	return fmt.Sprintf("%s/%s", u.Hostname(), p), nil
+}
